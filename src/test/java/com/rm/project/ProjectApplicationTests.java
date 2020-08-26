e18465c4e2868c45eacdd7e6dc70da372c2f9c55
sangmin <hsmang@naver.com>
2020-08-26 22:05:31 +0900

    Initial commit

---
+++ src/test/java/com/rm/project/ProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rm.project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
